cfd0ff91591ef8706d25d880a3bf96026ca64759
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-19 18:54:41 -0800

    Refactored the click functionality to eliminate the loop

--- noise.js
+++ noise.js
@@ -1,12 +1,18 @@
 $(document).ready(() => {
-  const notes = ["c", "d", "e", "f", "g", "a", "b"]
 
-  for (let note of notes){
-    $(`.${note}`).on('click', function(){
-      $(`#${note}Audio`).load();
-      $(`#${note}Audio`).get(0).play();
-    });
-  }
+  $('.instrument').on('click', '.note', function(){
+    $(`#${$(this).text()}Audio`).load();
+    $(`#${$(this).text()}Audio`).get(0).play();
+  });
+
+  // const notes = ["c", "d", "e", "f", "g", "a", "b"]
+  //
+  // for (let note of notes){
+  //   $(`.${note}`).on('click', function(){
+  //     $(`#${note}Audio`).load();
+  //     $(`#${note}Audio`).get(0).play();
+  //   });
+  // }
 
 
   $('body').on('keypress', function(event){
