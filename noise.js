f68ce8b2bae246759cdf2fa0f2231c095e4f92d0
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-19 17:01:42 -0800

    Refactored code to make it drier

--- noise.js
+++ noise.js
@@ -1,3 +1,16 @@
-$(document).ready( function() {
-  // your code here
-});
+$(document).ready(() => {
+  const notes = ["c", "d", "e", "f", "g", "a", "b"]
+
+  for (let note of notes){
+    $(`.${note}`).on('click', function(){
+      $(`#${note}Audio`).load();
+      $(`#${note}Audio`).get(0).play();
+    });
+  }
+
+
+  $('body').on('keypress', function(event){
+    $(`#${event.key}Audio`).load();
+    $(`#${event.key}Audio`).get(0).play();
+  });
+})
